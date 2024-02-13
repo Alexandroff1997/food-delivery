4ae61c2221e0c97d6b8e008ee40896d1a64a4d40
Alex <alex.symire1997@gmail.com>
2024-02-14 02:51:17 +0700

    update router syntax to new version, menu layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { Route, Routes } from 'react-router-dom';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
-import { Menu } from './pages/Menu/Menu';
-import { Cart } from './pages/Cart/Cart';
+
 
 function App() {
 	return (
@@ -10,15 +8,6 @@ function App() {
 			<Button appearence='big'>Кнопка</Button>
 			<Button appearence='small'>Кнопка</Button>
 			<Input placeholder='Email' />
-			<div>
-				<a href="/">Menu</a>
-				<a href="/cart">Cart</a>
-				<a href="*">Other</a>
-			</div>
-			<Routes>
-				<Route path='/' element={<Menu />} />
-				<Route path='/cart' element={<Cart />} />
-			</Routes>
 		</>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { MenuLayout } from './layout/MenuLayout/MenuLayout';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <MenuLayout />,
+		children: [
+			{
+				path: '/',
+				element: <Menu />
+			},
+			{
+				path: '/cart',
+				element: <Cart />
+			}
+		]
+	},
+	{
+		path: '/cart',
+		element: <Cart />
+	},
+	{
+		path: '*'
+	}
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
