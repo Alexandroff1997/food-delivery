5e6c6f389da903d204c75a6a4b070886bb8bf1a7
Alex <alex.symire1997@gmail.com>
2024-02-14 02:43:11 +0700

    pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { Route, Routes } from 'react-router-dom';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
 
 function App() {
 	return (
@@ -7,6 +10,15 @@ function App() {
 			<Button appearence='big'>Кнопка</Button>
 			<Button appearence='small'>Кнопка</Button>
 			<Input placeholder='Email' />
+			<div>
+				<a href="/">Menu</a>
+				<a href="/cart">Cart</a>
+				<a href="*">Other</a>
+			</div>
+			<Routes>
+				<Route path='/' element={<Menu />} />
+				<Route path='/cart' element={<Cart />} />
+			</Routes>
 		</>
 	);
 }
