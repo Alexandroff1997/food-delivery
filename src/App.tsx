dd2c2b5b1a9697a613053de41d83f763c22cedc9
Alex <alex.symire1997@gmail.com>
2024-02-14 02:24:28 +0700

    component input and input styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Button from './components/Button/Button';
+import Input from './components/Input/Input';
 
 function App() {
 	return (
 		<>
 			<Button appearence='big'>Кнопка</Button>
 			<Button appearence='small'>Кнопка</Button>
+			<Input placeholder='Email' />
 		</>
 	);
 }
