aa509a5a234d883007c5e3e65ab01cb821a2db68
Alex <alex.symire1997@gmail.com>
2024-02-14 22:28:19 +0700

    navlink router logic

--- src/layout/MenuLayout/MenuLayout.tsx
+++ src/layout/MenuLayout/MenuLayout.tsx
@@ -1,8 +1,10 @@
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import styles from './MenuLayout.module.css';
 import Button from '../../components/Button/Button';
+import cn from 'classnames';
 
 export function MenuLayout() {
+
 	return (
 		<div className={styles['layout']}>
 			<div className={styles['sidebar']}>
@@ -14,14 +16,18 @@ export function MenuLayout() {
 					<div className={styles['email']}>email@mail.ru</div>
 				</div>
 				<div className={styles['menu']}>
-					<Link to='/' className={styles['link']}>
+					<NavLink to='/' className={({ isActive }) => cn(styles['link'], {
+						[styles.active]: isActive
+					})}>
 						<img src="/menu.svg" alt="menu icon" />
 						Menu
-					</Link>
-					<Link to='/cart' className={styles['link']}>
+					</NavLink>
+					<NavLink to='/cart' className={({ isActive }) => cn(styles['link'], {
+						[styles.active]: isActive
+					})}>
 						<img src="/cart.svg" alt="cart icon" />
 						Cart
-					</Link>
+					</NavLink>
 				</div>
 				<Button className={styles['exit']}>
 					<img src="/exit.svg" alt="exit icon" />
